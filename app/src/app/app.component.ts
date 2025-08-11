99c73a341b68d918832f755d4ccebe7698b3b6b6
MtinDuarte <duartemartn@gmail.com>
2025-08-10 23:52:40 -0300

    Inicios: renombro componente raíz "component", creación componente "devices"

---
+++ app/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Devices } from "./devices/devices";
+
+/* Esto se denomina Decorador */
+@Component({                              /* Metadata.... */
+  selector: 'app-root',                   // Etiqueta (así se usa en html)
+  standalone : true,                      // No necesita módulos
+  imports: [RouterOutlet, Devices],       // Módulos que usa este componente
+  templateUrl: './app.component.html',    // Archivo template
+  styleUrl:    './app.component.css'      // Estilos
+})
+export class App {
+  protected title = 'app';
+}
---
+++ app/src/app/devices/devices.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-devices',
+  standalone : true,            // Este componente puede funcionar por si solo sin un "módulo"
+  imports: [],
+  templateUrl: './devices.html',
+  styleUrl: './devices.css'
+})
+export class Devices {
+
+}
