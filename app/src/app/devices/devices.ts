0763f5626c86e758c282a575c32f34704aa75aca
MtinDuarte <duartemartn@gmail.com>
2025-08-11 00:30:09 -0300

    Componentes: Bindings 1 y 2 vías.

--- app/src/app/devices/devices.ts
+++ app/src/app/devices/devices.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-devices',
   standalone : true,            // Este componente puede funcionar por si solo sin un "módulo"
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './devices.html',
   styleUrl: './devices.css'
 })
 export class Devices {
 
+  buttonState: boolean;
+  buttonText : string;
+  constructor()
+  {
+    this.buttonState = false;
+    this.buttonText = "Press me";
+  }
+
+  changeButtonState()
+  {
+    this.buttonState = !this.buttonState;
+  }
 }
